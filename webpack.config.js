f8e0f20744b17e93d8cee4f2f80c375ec366781b
Jesse Rusak <me@jesserusak.com>
2021-06-20 15:33:39 +0000

    working sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: {
         aframedemo: './tools_for_curiousity_app/t4c/js/aframedemo.js',
     },
+    devtool: 'source-map', // TODO: this works nicely in development for Safari + Chrome, but is slow. We should also change when building for production.
     output: {
         path: path.resolve(__dirname, './tools_for_curiousity_app/t4c/static/t4c/dist/'),
         publicPath: '/static/t4c/dist',
