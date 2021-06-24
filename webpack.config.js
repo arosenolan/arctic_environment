7f6b05ffb4bc371a89871b9d92dc94e20cee2da0
Jesse Rusak <me@jesserusak.com>
2021-06-24 01:18:36 +0000

    update for main changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        aframedemo: './tools_for_curiousity_app/t4c/js/aframedemo.js',
+        aframedemo: './tools_of_curiosity_app/t4c/js/aframedemo.js',
     },
     devtool: 'source-map', // TODO: this works nicely in development for Safari + Chrome, but is slow. We should also change when building for production.
     output: {
-        path: path.resolve(__dirname, './tools_for_curiousity_app/t4c/static/t4c/dist/'),
+        path: path.resolve(__dirname, './tools_of_curiosity_app/t4c/static/t4c/dist/'),
         publicPath: '/static/t4c/dist',
         filename: '[name].bundle.js'
     }
